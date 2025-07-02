f5181db2d336f77967768f9e5d2c05fea030c77d
anshu2298 <amshusingh2298@gmail.com>
2025-07-02 09:20:38 +0530

    m-changes

--- EMPLOYEE/src/utils/apiPaths.js
+++ EMPLOYEE/src/utils/apiPaths.js
@@ -1,25 +1,25 @@
-const BASE_URL = "http://localhost:3000/api";
-
+// const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://crm-backend-sooty.vercel.app";
 export const API_PATHS = {
   LEADS: {
     GET: (employeeId) =>
-      `${BASE_URL}/employee/${employeeId}/leads`,
-    UPDATE: (leadId) => `${BASE_URL}/leads/${leadId}`,
+      `${BASE_URL}/api/employee/${employeeId}/leads`,
+    UPDATE: (leadId) => `${BASE_URL}/api/leads/${leadId}`,
     SET_REMINDER: (leadId) =>
-      `${BASE_URL}/leads/${leadId}/reminder`,
+      `${BASE_URL}/api/leads/${leadId}/reminder`,
   },
   EMPLOYEE: {
-    LOGIN: `${BASE_URL}/employee/login`,
+    LOGIN: `${BASE_URL}/api/employee/login`,
     UPDATE: (employeeId) =>
-      `${BASE_URL}/employee/update/${employeeId}`,
+      `${BASE_URL}/api/employee/update/${employeeId}`,
     SET_ACTIVITY: (employeeId) =>
-      `${BASE_URL}/employee/activity/${employeeId}`,
+      `${BASE_URL}/api/employee/activity/${employeeId}`,
     UPDATE_SESSION: (employeeId) =>
-      `${BASE_URL}/employee/session/${employeeId}`,
+      `${BASE_URL}/api/employee/session/${employeeId}`,
     GET_ATTENDANCE: (employeeId) =>
-      `${BASE_URL}/employee/attendance/${employeeId}`,
+      `${BASE_URL}/api/employee/attendance/${employeeId}`,
   },
   NOTIFICATION: {
-    ADD: `${BASE_URL}/events/add-event`,
+    ADD: `${BASE_URL}/api/events/add-event`,
   },
 };
